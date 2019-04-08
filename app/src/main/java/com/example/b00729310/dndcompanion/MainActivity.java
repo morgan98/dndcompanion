699b4db2a870345e6245e94c1ac0ff7d2ac0a98f
Eammon Morgan <eammonmorgan@gmail.com>
2019-04-08 14:04:48 +0100

    Added code for changing between Fragments

--- app/src/main/java/com/example/b00729310/dndcompanion/MainActivity.java
+++ app/src/main/java/com/example/b00729310/dndcompanion/MainActivity.java
@@ -3,49 +3,85 @@ package com.example.b00729310.dndcompanion;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+//Import Fragments
+import com.example.b00729310.dndcompanion.homeFragment;
+import com.example.b00729310.dndcompanion.diceFragment;
+import com.example.b00729310.dndcompanion.createFragment;
+import com.example.b00729310.dndcompanion.loadFragment;
+import com.example.b00729310.dndcompanion.sessionFragment;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dice:
-                    mTextMessage.setText(R.string.title_dice);
-                    return true;
-                case R.id.navigation_create:
-                    mTextMessage.setText(R.string.title_create);
-                    return true;
-                case R.id.navigation_load:
-                    mTextMessage.setText(R.string.title_load);
-                    return true;
-                case R.id.navigation_session:
-                    mTextMessage.setText(R.string.title_session);
-                    return true;
-            }
-            return false;
-        }
-    };
-
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        toolbar = getSupportActionBar();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Load Home Fragment on Launch
+        toolbar.setTitle("Home");
+        loadFragment(new homeFragment());
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle("Home");
+                    fragment = new homeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_dice:
+                    toolbar.setTitle("Dice");
+                    fragment = new diceFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_create:
+                    toolbar.setTitle("Create");
+                    fragment = new createFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_load:
+                    toolbar.setTitle("Load");
+                    fragment = new loadFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_session:
+                    toolbar.setTitle("Session");
+                    fragment = new sessionFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    //Load Fragment method for handling the change of fragments
+    private void loadFragment(Fragment fragment)
+    {
+        //Load Fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
 }
