e94dc7182f71eeed67fc124f50b76d2e2a9e0334
Eammon Morgan <eammonmorgan@gmail.com>
2019-04-08 17:49:00 +0100

    Complete Nav

--- app/src/main/java/com/example/b00729310/dndcompanion/MainActivity.java
+++ app/src/main/java/com/example/b00729310/dndcompanion/MainActivity.java
@@ -10,78 +10,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-//Import Fragments
-import com.example.b00729310.dndcompanion.homeFragment;
-import com.example.b00729310.dndcompanion.diceFragment;
-import com.example.b00729310.dndcompanion.createFragment;
-import com.example.b00729310.dndcompanion.loadFragment;
-import com.example.b00729310.dndcompanion.sessionFragment;
 
 
-public class MainActivity extends AppCompatActivity {
 
-    private ActionBar toolbar;
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected  void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)  findViewById(R.id.navigation);
 
-        toolbar = getSupportActionBar();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+                switch (menuItem.getItemId())
+                {
+                    case R.id.navigation_home:
+                        selectedFragment = new homeFragment();
+                        break;
+                    case R.id.navigation_dice:
+                        selectedFragment = new diceFragment();
+                        break;
+                    case R.id.navigation_create:
+                        selectedFragment = new createFragment();
+                        break;
+                    case R.id.navigation_load:
+                        selectedFragment = new loadFragment();
+                        break;
+                    case R.id.navigation_session:
+                        selectedFragment = new sessionFragment();
+                        break;
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        //Load Home Fragment on Launch
-        toolbar.setTitle("Home");
-        loadFragment(new homeFragment());
-    }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment fragment;
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    toolbar.setTitle("Home");
-                    fragment = new homeFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_dice:
-                    toolbar.setTitle("Dice");
-                    fragment = new diceFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_create:
-                    toolbar.setTitle("Create");
-                    fragment = new createFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_load:
-                    toolbar.setTitle("Load");
-                    fragment = new loadFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_session:
-                    toolbar.setTitle("Session");
-                    fragment = new sessionFragment();
-                    loadFragment(fragment);
-                    return true;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_container, selectedFragment);
+                transaction.commit();
+                return true;
             }
-            return false;
-        }
-    };
 
-    //Load Fragment method for handling the change of fragments
-    private void loadFragment(Fragment fragment)
-    {
-        //Load Fragment
+        });
+        //Display Home Fragment on Launch
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_container, fragment);
-        transaction.addToBackStack(null);
+        transaction.replace(R.id.frame_container, homeFragment.newInstance());
         transaction.commit();
 
-    }
-}
+
+
+
+    }//end OnCreate
+
+
+}//End Main Activity
