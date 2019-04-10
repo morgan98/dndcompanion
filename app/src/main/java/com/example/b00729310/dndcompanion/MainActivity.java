3708d6b1e427e4753efb923b5bb44430999dd5e8
Eammon Morgan <eammonmorgan@gmail.com>
2019-04-10 16:48:07 +0100

    Finished Backend of Character Creator

--- app/src/main/java/com/example/b00729310/dndcompanion/MainActivity.java
+++ app/src/main/java/com/example/b00729310/dndcompanion/MainActivity.java
@@ -22,7 +22,13 @@ public class MainActivity extends AppCompatActivity
 
     //Var for handling adding or subtracting from character stat
     private int strCount = 8;
+    private int dexCount= 8;
+    private int conCount = 8;
+    private int intCount = 8;
+    private int wisCount = 8;
+    private int chaCount = 8;
 
+    //Var for handling the unused points
     private  int remCount = 27;
 
     int BaseValue = 8;
@@ -72,7 +78,6 @@ public class MainActivity extends AppCompatActivity
         transaction.commit();
 
 
-        //Generate Calls to Toast Messages
 
 
         }//end OnCreate
@@ -103,7 +108,10 @@ public class MainActivity extends AppCompatActivity
         {
 
             strCount = strCount ;
-            remCount = remCount -1 ;
+            updateStr(strCount);
+
+            remCount = remCount;
+            updateRem(remCount);
 
             Toast error1 = Toast.makeText(this,R.string.toastErrorNoPoints, Toast.LENGTH_LONG);
             error1.show();
@@ -133,16 +141,384 @@ public class MainActivity extends AppCompatActivity
             strCount = strCount;
             updateStr(strCount);
 
-            strCount = strCount ;
+            remCount = remCount ;
+            updateRem(remCount);
+
+            Toast advice2 = Toast.makeText(this,R.string.lowestvalue, Toast.LENGTH_LONG);
+            advice2.show();
+
+        }
+        else if(strCount > 16)
+        {
+            strCount = strCount - 2;
+            remCount = remCount - 2;
+        }
+    }
+
+    //Methods for adding amd removing Dex
+    public void addDex(View view)
+    {
+
+        if(remCount > 0 && dexCount < 16) {
+            dexCount = dexCount + 1;
+            updateDex(dexCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(dexCount >= 16 && remCount != 0)
+        {
+            dexCount = dexCount + 2;
+            updateDex(dexCount);
+
+            remCount = remCount - 2;
+            updateRem(remCount);
+
+        }
+        else if(remCount == 0)
+        {
+
+            dexCount = dexCount;
+            updateDex(dexCount);
+            remCount = remCount;
+            updateRem(remCount);
+
+            Toast error1 = Toast.makeText(this,R.string.toastErrorNoPoints, Toast.LENGTH_LONG);
+            error1.show();
+
+        }
+
+        if(dexCount ==16)
+        {
+            Toast statadvice = Toast.makeText(this,R.string.advice1, Toast.LENGTH_LONG);
+            statadvice.show();
+        }
+
+    }
+
+    public void minusDex(View view)
+    {
+        if (dexCount != 8 && dexCount < 16)
+        {
+            dexCount = dexCount - 1;
+            updateDex(dexCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(dexCount == 8)
+        {
+            dexCount = dexCount;
+            updateDex(dexCount);
+
+            remCount = remCount ;
+            updateRem(remCount);
+
+            Toast advice2 = Toast.makeText(this,R.string.lowestvalue, Toast.LENGTH_LONG);
+            advice2.show();
+
+        }
+        else if(strCount > 16)
+        {
+            dexCount = dexCount - 2;
+            updateDex(dexCount);
+            remCount = remCount - 2;
+            updateRem(remCount);
+        }
+    }
+
+    //Con Methods
+    public void addCon(View view)
+    {
+
+        if(remCount > 0 && conCount < 16) {
+            conCount = conCount + 1;
+            updateCon(conCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(conCount >= 16 && remCount != 0)
+        {
+            conCount = conCount + 2;
+            updateCon(conCount);
+
+            remCount = remCount - 2;
+            updateRem(remCount);
+
+        }
+        else if(remCount == 0)
+        {
+
+            conCount = conCount ;
+            updateCon(conCount);
+            remCount = remCount -1 ;
+            updateRem(remCount);
+
+            Toast error1 = Toast.makeText(this,R.string.toastErrorNoPoints, Toast.LENGTH_LONG);
+            error1.show();
+
+        }
+
+        if(conCount ==16)
+        {
+            Toast statadvice = Toast.makeText(this,R.string.advice1, Toast.LENGTH_LONG);
+            statadvice.show();
+        }
+
+    }
+
+    public void minusCon(View view)
+    {
+        if (conCount != 8 && conCount < 16)
+        {
+            conCount = conCount - 1;
+            updateCon(conCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(strCount == 8)
+        {
+            conCount = conCount;
+            updateCon(conCount);
+
+            conCount = conCount ;
+            updateRem(conCount);
+            remCount = remCount ;
+            updateRem(remCount);
+
+            Toast advice2 = Toast.makeText(this,R.string.lowestvalue, Toast.LENGTH_LONG);
+            advice2.show();
+
+        }
+        else if(conCount > 16)
+        {
+            conCount = conCount - 2;
+            remCount = remCount - 2;
+        }
+    }
+
+
+
+    //Int Methods
+    public void addInt(View view)
+    {
+
+        if(intCount > 0 && intCount < 16) {
+            intCount = intCount + 1;
+            updateInt(intCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(intCount >= 16 && intCount != 0)
+        {
+            intCount = intCount + 2;
+            updateInt(intCount);
+
+            remCount = remCount - 2;
+            updateRem(remCount);
+
+        }
+        else if(remCount == 0)
+        {
+
+            intCount = intCount ;
+            updateInt(intCount);
+            remCount = remCount -1 ;
+            updateRem(remCount);
+
+            Toast error1 = Toast.makeText(this,R.string.toastErrorNoPoints, Toast.LENGTH_LONG);
+            error1.show();
+
+        }
+
+        if(intCount ==16)
+        {
+            Toast statadvice = Toast.makeText(this,R.string.advice1, Toast.LENGTH_LONG);
+            statadvice.show();
+        }
+
+    }
+
+    public void minusInt(View view)
+    {
+        if (intCount != 8 && intCount < 16)
+        {
+            intCount = intCount - 1;
+            updateInt(intCount);
+
+            intCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(strCount == 8)
+        {
+            intCount = intCount;
+            updateInt(intCount);
+
+            intCount = intCount ;
+            updateInt(intCount);
+            remCount = remCount;
+            updateRem(remCount);
+
+            Toast advice2 = Toast.makeText(this,R.string.lowestvalue, Toast.LENGTH_LONG);
+            advice2.show();
+
+        }
+        else if(intCount > 16)
+        {
+            intCount = intCount - 2;
+            updateInt(intCount);
+            remCount = remCount - 2;
+            updateRem(remCount);
+        }
+    }
+
+    //Wiz Methods
+    public void addWis(View view)
+    {
+
+        if(wisCount > 0 && wisCount < 16) {
+            wisCount = wisCount + 1;
+            updateWis(wisCount);
+
+            wisCount = wisCount - 1;
+            updateRem(remCount);
+        }
+        else if(wisCount >= 16 && wisCount != 0)
+        {
+            wisCount = wisCount + 2;
+            updateStr(strCount);
+
+            remCount = remCount - 2;
+            updateRem(remCount);
+
+        }
+        else if(remCount == 0)
+        {
+
+            wisCount = wisCount ;
+            updateWis(wisCount);
+            remCount = remCount -1 ;
+            updateRem(remCount);
+
+            Toast error1 = Toast.makeText(this,R.string.toastErrorNoPoints, Toast.LENGTH_LONG);
+            error1.show();
+
+        }
+
+        if(wisCount ==16)
+        {
+            Toast statadvice = Toast.makeText(this,R.string.advice1, Toast.LENGTH_LONG);
+            statadvice.show();
+        }
+
+    }
+
+    public void minusWis(View view)
+    {
+        if (wisCount != 8 && wisCount < 16)
+        {
+            wisCount = wisCount - 1;
+            updateWis(wisCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(wisCount == 8)
+        {
+            wisCount = wisCount;
+            updateWis(wisCount);
+
+
             remCount = remCount ;
 
             Toast advice2 = Toast.makeText(this,R.string.lowestvalue, Toast.LENGTH_LONG);
             advice2.show();
 
         }
+        else if(wisCount > 16)
+        {
+            wisCount = wisCount - 2;
+            updateWis(wisCount);
+            remCount = remCount - 2;
+            updateRem(remCount);
+        }
     }
 
+    //Int Methods
+    public void addCha(View view)
+    {
 
+        if(chaCount > 0 && chaCount < 16) {
+            chaCount = chaCount + 1;
+            updateCha(chaCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(chaCount >= 16 && chaCount != 0)
+        {
+            chaCount = chaCount + 2;
+            updateStr(chaCount);
+
+            remCount = remCount - 2;
+            updateRem(chaCount);
+
+        }
+        else if(remCount == 0)
+        {
+
+            chaCount = chaCount ;
+            updateCha(chaCount);
+            remCount = remCount -1 ;
+            updateRem(remCount);
+
+            Toast error1 = Toast.makeText(this,R.string.toastErrorNoPoints, Toast.LENGTH_LONG);
+            error1.show();
+
+        }
+
+        if(chaCount ==16)
+        {
+            Toast statadvice = Toast.makeText(this,R.string.advice1, Toast.LENGTH_LONG);
+            statadvice.show();
+        }
+
+    }
+
+    public void minusCha(View view)
+    {
+        if (chaCount != 8 && chaCount < 16)
+        {
+            chaCount = chaCount - 1;
+            updateCha(chaCount);
+
+            remCount = remCount - 1;
+            updateRem(remCount);
+        }
+        else if(chaCount == 8)
+        {
+            chaCount = conCount;
+            updateCha(chaCount);
+
+
+            remCount = remCount ;
+            updateRem(remCount);
+
+            Toast advice2 = Toast.makeText(this,R.string.lowestvalue, Toast.LENGTH_LONG);
+            advice2.show();
+
+        }
+        else if(chaCount > 16)
+        {
+            chaCount = chaCount - 2;
+            updateCha(chaCount);
+            remCount = remCount - 2;
+            updateRem(remCount);
+        }
+    }
 
 
 
@@ -153,6 +529,41 @@ public class MainActivity extends AppCompatActivity
         //Update the Strength Value
         TextView updateStr = (TextView) findViewById(R.id.strValue);
         updateStr.setText(""+ num);
+    }
+
+    private void updateDex(int num)
+    {
+        //Update the Strength Value
+        TextView updateDex = (TextView) findViewById(R.id.dexValue);
+        updateDex.setText(""+ num);
+    }
+
+    private void updateCon(int num)
+    {
+        //Update the Strength Value
+        TextView updateCon = (TextView) findViewById(R.id.conValue);
+        updateCon.setText(""+ num);
+    }
+
+    private void updateInt(int num)
+    {
+        //Update the Strength Value
+        TextView updateInt = (TextView) findViewById(R.id.intValue);
+        updateInt.setText(""+ num);
+    }
+
+    private void updateWis(int num)
+    {
+        //Update the Strength Value
+        TextView updateWis = (TextView) findViewById(R.id.wisValue);
+        updateWis.setText(""+ num);
+    }
+
+    private void updateCha(int num)
+    {
+        //Update the Strength Value
+        TextView updateCha = (TextView) findViewById(R.id.charValue);
+        updateCha.setText(""+ num);
     }
 
 
